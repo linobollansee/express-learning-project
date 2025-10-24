3e3200eab73bec658c92635cc8a79b3de7408c83
linobollansee <linobollansee@hotmail.com>
2025-10-24 15:37:16 +0200

    Update homeController.js

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,26 @@
+// Import the user model, which handles user data (e.g., listing users)
 const userModel = require('../models/userModel');
 
+/**
+ * Controller method for the home page ('/').
+ * - Renders the 'index' view/template.
+ * - Passes a title and the list of users from the user model to the view.
+ */
 exports.index = (req, res) => {
-  res.render('index', { title: 'Express Learning Project', users: userModel.list() });
+  res.render('index', { 
+    title: 'Express Learning Project',  // Page title displayed in the view
+    users: userModel.list()             // List of users fetched from the model
+  });
 };
 
+/**
+ * Controller method for the users page ('/users').
+ * - Renders the 'users' view/template.
+ * - Passes a title and the list of users from the user model to the view.
+ */
 exports.usersPage = (req, res) => {
-  res.render('users', { title: 'Users', users: userModel.list() });
+  res.render('users', { 
+    title: 'Users',                     // Page title displayed in the view
+    users: userModel.list()             // List of users fetched from the model
+  });
 };
